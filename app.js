acac217e1cecf39e6b612a729581502df0ed0144
Gabriel Mochi Ribeiro <gmochi56@icloud.com>
2019-11-04 23:27:22 -0200

    getting credentials by environment variables

--- app.js
+++ app.js
@@ -4,20 +4,36 @@ const bodyParser = require('body-parser')
 const basicAuth = require('express-basic-auth')
 
 const app = express()
+const users = {}
 
 app.use(cors({ origin: true }))
 app.use(bodyParser.json())
 
+/**
+ * Get the username and respective password
+ * by environemt variables. That way is easier
+ * to change the valid credentials values
+ * without the necessity to chnage the code.
+ * 
+ * Create a .env file if it does not exists.
+ */
+users[process.env.BASIC_AUTH_USERNAME] = process.env.BASIC_AUTH_PASSWORD
+
 app.use(basicAuth({
-  users: { 'root': 'toor' },
+  users,
   challenge: true,
   realm: 'Imb4T3st4pp',
+  // Return an unathorized message if credentials
+  // are invalid or not provided.
   unauthorizedResponse: req => req.auth
     ? 'Credentials invalid!'
     : 'No credentials provided!'
 }))
 
 app.get('/', (req, res) => {
+  // In case of success authentication, show
+  // the welcome message to the authenticated
+  // user.
   res.send(`Welcome ${req.auth.user}!`)
 })
 
